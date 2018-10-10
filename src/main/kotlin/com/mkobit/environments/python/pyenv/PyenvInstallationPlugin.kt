b53bec2131c5a307811df3b0f84fff738e1c6dc3
Mike Kobit <mkobit@gmail.com>
2018-10-10 10:39:19 -0500

    Move `pyenv` stuff into its own directory in the build directory

--- src/main/kotlin/com/mkobit/environments/python/pyenv/PyenvInstallationPlugin.kt
+++ src/main/kotlin/com/mkobit/environments/python/pyenv/PyenvInstallationPlugin.kt
@@ -30,26 +30,25 @@ class PyenvInstallationPlugin @Inject constructor(
       val containerType = object : TypeOf<NamedDomainObjectContainer<PyenvInstallation>>() {}
       val container = container { name ->
         val pyenvVersion = objectFactory.property<String>()
+        val pyenvBaseDirectory = layout.buildDirectory.dir("pythonEnvironments/pyenv")
         val downloadPyenvInstallerBundle = tasks.register<DownloadPyenvBundle>("downloadPyenvInstalledBundle$name") {
           group = PYENV_TASK_GROUP
           // TODO: support non-github downloads for self-hosting
           distributionUrl.set(pyenvVersion.map { "https://github.com/pyenv/pyenv/archive/$it.tar.gz" })
-          // TODO: isolate pyenv stuff into own directory
-          destination.set(layout.buildDirectory.file(pyenvVersion.map { "pyenv-$it.tar.gz" }))
+          destination.set(pyenvBaseDirectory.flatMap { base -> base.file(pyenvVersion.map { "pyenv-$it.tar.gz" })})
         }
 
         val decompressPyenvInstallerBundle = tasks.register<DecompressPyenvBundle>("installPyenv$name") {
           group = PYENV_TASK_GROUP
           bundle.set(downloadPyenvInstallerBundle.get().destination)
-          // TODO: isolate pyenv stuff into its own directory
           destination.set(layout.buildDirectory.dir(pyenvVersion.map { "pyenv-$it" }))
+          destination.set(pyenvBaseDirectory.flatMap { base -> base.dir(pyenvVersion.map { "pyenv-$it" })})
         }
 
         // 'bin/pyenv' (at least in recent versions) symlinks to the
         // Unfortunately Gradle doesn't seem to preserve the symlink from unpacking
         // https://github.com/gradle/gradle-native/issues/294
-        // TODO: use flatMap from gradle 5.0 to not force configuration
-        val pyenv = decompressPyenvInstallerBundle.map { install -> install.destination.file("libexec/pyenv") }.get()
+        val pyenv = decompressPyenvInstallerBundle.flatMap { install -> install.destination.file("libexec/pyenv") }
         PyenvInstallation(
             name,
             pyenv,
